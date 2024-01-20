676b877384f648c484bb0efffaca87a188a8f7f0
sukhmancode <coder4614@gmail.com>
2024-01-21 02:17:33 +0530

    changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Nav from '../components/nav'
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
-
+import { FaPhoneAlt,FaWpforms  } from "react-icons/fa";
 
 const Contact:React.FC= () => {
+    
   return (
     <div className='contact'>
         <Nav/>
@@ -18,20 +19,54 @@ const Contact:React.FC= () => {
 
         <div className="contact-icons">
             <div className="contact-location">
-                <div>
+                <div className='icon-parent'>
+                    <div className="email-icon">
                 <FaLocationDot fill='tomato' size={30}/>
+                     </div>
+                <p>123 Street, New York, USA</p>
                 </div>
             </div>
 
             <div className="contact-email">
-                <div className="email-icon">
+                <div className='icon-parent'>
+                    <div className="email-icon">
                     <MdEmail fill='tomato' size={30}/>
+                    </div>
+                    <p>info@example.com</p>
                 </div>
             </div>
             <div className="contact-phone">
-                <div className="contact-icon">
-                    
+                <div className='icon-parent'>
+                    <div className="email-icon">
+                    <FaPhoneAlt fill='tomato' size={30}/>
+                    </div>
+                    <p>+012 345 6789</p>
                 </div>
+            </div>
+        </div>
+
+        <div className="form-area">
+            <div className='form'>
+            <div className='form-heading'>
+            <FaWpforms size={30}/>
+            <p className='fill-details-txt'> Please fill your details</p>
+            </div>
+            <form>
+                <div className="name-email">
+                    <input type="text" placeholder='Your Name'/>
+                    <input type="text" placeholder='Age of Child' />
+                </div>
+                <div className='subjects'> 
+                    <input type="text" placeholder='Subject' />
+                    <textarea placeholder='Message'></textarea>
+                </div>
+                <div>
+                    <input type="submit" value={'Send Message'} />
+                </div>
+            </form>
+            </div>
+            <div className="map">
+                
             </div>
         </div>
     </div>
